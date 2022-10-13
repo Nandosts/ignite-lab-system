b2b4552c2dfc6e8d917c09aabc7f2268777feb75
Fernando Melo <fernando.jsts@gmail.com>
2022-10-13 13:04:26 -0300

    :rewind: Use yarn and fix config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
@@ -14,7 +15,9 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/ignite-lab-system";
+      config.base = "/ignite-lab-system/";
     }
+
+    return config;
   },
 };
