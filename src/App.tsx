568686cbb81181c3e9cb2b5b2fa2cc9ca603fcce
Fernando Melo <fernando.jsts@gmail.com>
2022-10-13 09:47:23 -0300

    :package: Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import React from "react";
+import "./styles/global.css";
+
 export function App() {
   return (
-    <div className="App">
+    <div>
+      <h1 className="text-5xl font-bold text-violet-500">hello world</h1>
+
+      <button className="rounded bg-violet-500  px-4 py-2 font-medium text-white hover:bg-violet-600">
+        Botão
+      </button>
     </div>
-  )
+  );
 }
