cf883000986549e59a7fe0c32d0e03cb3eef369f
Fernando Melo <fernando.jsts@gmail.com>
2022-10-13 09:49:02 -0300

    :lipstick: Add default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
